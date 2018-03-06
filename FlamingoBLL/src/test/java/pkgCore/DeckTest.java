fed006d394ed106edd5cd7d8e6b1183ccfcf95fb
BenSegal855 <bensegal@udel.edu>
2018-03-06 16:52:07 -0500

    Fixed a thing

--- FlamingoBLL/src/test/java/pkgCore/DeckTest.java
+++ FlamingoBLL/src/test/java/pkgCore/DeckTest.java
@@ -14,7 +14,7 @@ public class DeckTest {
 		assertTrue(d1.getSize() == 52);
 		//	DONE: Build a deck(6), make sure there are 312 cards in the deck 
 		Deck d2 = new Deck(6);
-		assertTrue(d1.getSize() == 312);
+		assertTrue(d2.getSize() == 312);
 	}
 
 }
